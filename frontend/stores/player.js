543643d87e18b39250ed3fed4b066cf5ed0b1518
Pawan Rai <pawandeeprai@gmail.com>
2016-04-14 13:13:31 -0700

    created player store

---
+++ frontend/stores/player.js
@@ -0,0 +1,33 @@
+var Store = require('flux/utils').Store;
+var AppDispatcher = require('../dispatcher/dispatcher.js');
+
+var PlayerStore = new Store(AppDispatcher);
+
+var players_ = [];
+
+function Player(name, color){
+  this.name = name;
+  this.color = color;
+  this.resources = {
+    wood: 0,
+    brick: 0,
+    sheep: 0,
+    wheat: 0,
+    stone: 0
+  };
+}
+
+var generatePlayer = function(name, color){
+  players_.push(new Player(name,color));
+};
+
+
+PlayerStore.__onDispatch = function(payload){
+  switch(payload.actionType){
+    case "NEW_PLAYERS":
+      payload.players.forEach(function(player){
+        generatePlayer(player.name,player.color);
+      });
+      break;
+  }
+};
